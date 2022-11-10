307783b588e86e3718120f54234a0066c0657882
Endrit <endrittafilaj03@gmail.com>
2022-11-10 05:28:36 -0800

    Entities For User Restaurant Reservation

---
+++ backend/Domain/Entities/Reservation.cs
@@ -0,0 +1,19 @@
+
+
+namespace Reservation{
+
+    public class Reservation{
+
+        public int PhoneNumber{get; set;}
+        
+        public string Email{get; set;}
+
+        public DateTime StartDateTime{get; set;}
+
+        public int NumberOfPersons{get; set;}
+
+        public string Status{get; set;}
+
+        public string Description{get; set;}
+    }
+}
---
+++ backend/Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+
+
+namespace Domain{
+
+
+    public class User{
+ 
+         public string LastName {get; set; }
+
+         public int PhoneNumber{get; set;}
+
+         public string Email{get; set;}
+
+         public int Age {get; set;}
+
+        
+    }
+}
