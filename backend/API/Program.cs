b2003f7571d6538e8faaaadc06d589a901429071
Endrit <endrittafilaj03@gmail.com>
2022-11-11 03:57:05 -0800

    ConncectionString

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
