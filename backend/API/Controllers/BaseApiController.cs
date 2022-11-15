35fa3b84c8a7bf94060de1cb876dcc60ebb0fdeb
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2022-11-15 13:25:58 +0100

    Commit

--- backend/API/Controllers/BaseApiController.cs
+++ backend/API/Controllers/BaseApiController.cs
@@ -12,12 +12,8 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
        private IMediator _mediator;
 
        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
-    
-        
-        
     }
 }
