992a8a89886fc7e500d9ca2d1225dc3f777ec8de
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2022-11-15 12:53:31 +0100

    commit

--- backend/Persistence/Migrations/20221111141554_InitialCreate.Designer.cs
+++ backend/Persistence/Migrations/20221111141554_InitialCreate.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Presistence;
+using Persistence;
 
 #nullable disable
 
