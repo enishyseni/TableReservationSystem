88093b0b7e598b1de98167e3863eaf092099a430
Artin Rexhepi <66282654+artinrexhepii@users.noreply.github.com>
2022-11-17 13:41:52 +0100

    Height and Width updated

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,8 +8,8 @@ function Login() {
      
      <div className='main flex flex-col justify-evenly items-center'>
         
-        <div className=" flex flex-col items-center container h-96 w-96 bg-gray-900 bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
-        <div className="font-poppins h-35 w-60 text-white bg-black text-2xl tracking-wider py-2 px-5 rounded-full -mt-3"><span className='px-12'>Welcome</span></div>
+        <div className=" flex flex-col items-center container h-[444px] w-[444px] bg-gray-900 bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
+        <div className="font-poppins h-35 w-64 text-white bg-black text-2xl tracking-wider py-2 px-5 rounded-full -mt-3"><span className='px-12'>Welcome</span></div>
           <form className="h-full flex flex-col justify-evenly items-center">
             
               <input type="text" placeholder="Email or phone number" className="font-poppins text-slate-900 bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 border-black tracking-wider"/>
