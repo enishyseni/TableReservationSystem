8abb50c4b7b77bdbb2b8d79aa7cf4e67708f0206
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2022-11-21 16:07:41 +0100

    fixed migrations

--- backend/Persistence/Migrations/DataContextModelSnapshot.cs
+++ backend/Persistence/Migrations/DataContextModelSnapshot.cs
@@ -3,7 +3,6 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
@@ -12,11 +11,9 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20221121142656_InitialCreate")]
-    partial class InitialCreate
+    partial class DataContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
