202b2d14ae1949666be6a27bfb0e0c9e0120cb98
Artin Rexhepi <66282654+artinrexhepii@users.noreply.github.com>
2022-11-24 12:54:11 +0100

    Responsive

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-const register = () => {
-    
+import {BiArrowBack} from 'react-icons/bi';
+import {useNavigate} from "react-router-dom";
+function Register(){
+  const back = useNavigate ();
     return (
         <div className="App bg-gray-50 h-screen w-screen relative overflow-hidden flex flex-col justify-center items-center">
        
      
      <div className='main flex flex-col flex-initial justify-evenly items-center'>
         
-        <div className=" flex flex-col items-center container h-[444px] w-[444px] bg-gray-900 bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
+        <div className=" flex flex-col items-center container h-96 md:h-[444px] md:w-[444px] bg-gray-900 bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
         <div className="font-poppins h-35 w-64 text-white bg-black text-2xl tracking-wider py-2 px-5 rounded-full -mt-3"><span className='px-12'>Register</span></div>
           <form className="h-full w-5/6 flex flex-col justify-evenly items-stretch">
             <div className="name-surname flex justify-between ">
@@ -28,7 +30,11 @@ const register = () => {
             <input type="text" placeholder="Email or phone number" className="font-poppins text-slate-900 bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 border-black tracking-wider"/>
             <input type="password" placeholder= "Password" className="font-poppins text-slate-900 bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 border-black tracking-wider"/>
             <input type="password" placeholder= "Confirm password" className="font-poppins text-slate-900 bg-transparent focus:outline-none border border-r-0 border-t-0 border-l-0 border-black tracking-wider"/>
-            <button  className='font-poppins text-white bg-black cursor-pointer text-xl rounded-full px-5 py-1 bg-opacity-60 hover:bg-black hover:bg-opacity-80 tracking-wider'>Confirm</button>
+            <div className="flex justify-around">
+            <button onClick={() => back("/login")}><BiArrowBack/></button>
+            <button  className='font-poppins text-white bg-black w-[115px] cursor-pointer text-xl rounded-full px-5 py-1 bg-opacity-60 hover:bg-black hover:bg-opacity-80 tracking-wider'>Confirm</button>
+            </div>
+            
           </form>
         </div>
     </div>    
@@ -36,4 +42,4 @@ const register = () => {
     )
 };
 
-export default register;
+export default Register;
