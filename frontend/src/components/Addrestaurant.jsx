786a9ee65e81bbac2539c1544643b456d78280ff
Artin Rexhepi <66282654+artinrexhepii@users.noreply.github.com>
2022-11-25 12:38:44 +0100

    Routes

--- frontend/src/components/Addrestaurant.jsx
+++ frontend/src/components/Addrestaurant.jsx
@@ -78,7 +78,7 @@ function Addrestaurant () {
                     </div>
                     <div className="flex w-[100%] items-center justify-center ">
                     <div className=" m-2 px-2 w-[100%] flex items-center justify-center md:w-[80%] lg:w-[80%]"> 
-                        <div class="flex items-center justify-center w-full">
+                        <div class="flex items-center justify-center w-full border border-gray-300 rounded-md">
                             <label for="dropzone-file" class="flex flex-col items-center justify-center w-[100%] h-64  border-gray-300  rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                                 <div class="flex flex-col items-center justify-center pt-5 pb-6">
                                     <svg aria-hidden="true" class="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
